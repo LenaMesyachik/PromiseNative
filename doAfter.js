ff6ef494708ba0db4812d5f02c5497db2c1a07be
Lena <91722652+LenaMesyachik@users.noreply.github.com>
2022-01-29 00:02:25 +0300

    task7

--- doAfter.js
+++ doAfter.js
@@ -2,7 +2,11 @@ const doAfter = (seconds) => {
     let myPromise = new Promise((resolve, reject) => {
         setTimeout(() => {
             const randValue = Math.random()
-            reject(randValue)
+            if (randValue>0.5 ) {
+                resolve (randValue)
+            }else {
+                reject(randValue)
+            }
         }, seconds*1000)
     })
     return myPromise
