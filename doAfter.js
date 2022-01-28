1428e0e3fb3099734f2e450a702062b36742add2
Lena <91722652+LenaMesyachik@users.noreply.github.com>
2022-01-28 23:56:12 +0300

    task6 reject() and .catch

--- doAfter.js
+++ doAfter.js
@@ -2,7 +2,7 @@ const doAfter = (seconds) => {
     let myPromise = new Promise((resolve, reject) => {
         setTimeout(() => {
             const randValue = Math.random()
-            resolve(randValue)
+            reject(randValue)
         }, seconds*1000)
     })
     return myPromise
