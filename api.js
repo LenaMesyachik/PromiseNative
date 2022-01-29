be29f07af51b7a289da739e297fad5e1e0533d36
Lena <91722652+LenaMesyachik@users.noreply.github.com>
2022-01-29 18:38:43 +0300

    task9

--- api.js
+++ api.js
@@ -1,10 +1,14 @@
 const api = {
     sendStudentsCountToItKamasutra(studentsCount) {
-        return axios.post("https://it-kamasutra.com/students-need", 
-        { count: studentsCount });
+        return axios.post("https://it-kamasutra.com/students-need",
+            {count: studentsCount});
     },
     getVacanciesCountFromMicrosoft() {
-        return axios.get("https://microsoft.com");
+        const pr = axios.get("https://microsoft.com");
+        let pr2 = pr.then(res => {
+            return res.data.vacancies
+        })
+        return pr2
     },
     getVacanciesCountFromGoogle() {
         return axios.get("https://google.com");
